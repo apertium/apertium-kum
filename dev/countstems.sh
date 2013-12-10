4b1e27119ea5d7404c7a7c99103ed2750bc9bbe6
Jonathan North Washington <jonathan.n.washington@gmail.com>
2013-12-10 15:09:13 +0000

    44 verbs in с

--- dev/countstems.sh
+++ dev/countstems.sh
@@ -2,23 +2,23 @@
 
 LEXC=../apertium-kum.kum.lexc
 
-allcounts=`hfst-lexc --format foma $LEXC -o /dev/null 2>&1 | grep Root | sed 's/,/\n/g'`
+allcounts=`hfst-lexc --format foma $LEXC -o /dev/null 2>&1 | grep Root | sed -E 's/,/\n/g'`
 
 for line in $allcounts; do
-	thing=`echo $line | sed -r 's/(.*)\.\.\.([0-9]*),?/\1/'`;
-	numbr=`echo $line | sed -r 's/(.*)\.\.\.([0-9]*),?/\2/'`;
+	thing=`echo $line | sed -E 's/(.*)\.\.\.([0-9]*),?/\1/'`;
+	numbr=`echo $line | sed -E 's/(.*)\.\.\.([0-9]*),?/\2/'`;
 	if [[ "$thing" == "Root" ]]; then
 		rootsize=$numbr;
 		root=`grep -A$rootsize Root $LEXC | grep -v Root`;
 	fi;
 done;
 
-root=`echo $root | sed -r 's/\s*;/\n/g'`;
+root=`echo $root | sed -E 's/\s*;/\n/g'`;
 
 string=""
 for line in $allcounts; do
-	thing=`echo $line | sed -r 's/(.*)\.\.\.([0-9]*),?/\1/'`;
-	numbr=`echo $line | sed -r 's/(.*)\.\.\.([0-9]*),?/\2/'`;
+	thing=`echo $line | sed -E 's/(.*)\.\.\.([0-9]*),?/\1/'`;
+	numbr=`echo $line | sed -E 's/(.*)\.\.\.([0-9]*),?/\2/'`;
 	for thingname in $root; do
 		if [ "$thing" = "$thingname" ]; then
 			string="$string + $numbr";
@@ -26,4 +26,5 @@ for line in $allcounts; do
 	done;
 done;
 
+echo $string
 echo `calc $string`;
